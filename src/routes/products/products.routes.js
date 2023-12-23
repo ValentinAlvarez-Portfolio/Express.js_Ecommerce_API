8d67b2320eb06be5f45638a6ad215824b663fecc
Dne <valentinalvarez1998@gmail.com>
2023-12-22 21:55:15 -0300

    Se modifica el path del getById

--- src/routes/products/products.routes.js
+++ src/routes/products/products.routes.js
@@ -13,7 +13,7 @@ import {
 const productsRouter = Router();
 
 productsRouter.get('/', ProductsController.getAll);
-productsRouter.get('/:id', ProductsController.getById);
+productsRouter.get('/:_id', ProductsController.getById);
 productsRouter.post('/', premiumMiddleware, ProductsController.addOne);
 productsRouter.put('/:id', premiumMiddleware, ProductsController.updateOne);
 productsRouter.delete('/:id', premiumMiddleware, ProductsController.deleteOne);
