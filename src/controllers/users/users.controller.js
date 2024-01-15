d7c513ad3ce746567849db76f670eecf00c01bf8
Dne <valentinalvarez1998@gmail.com>
2024-01-15 01:51:59 -0300

    Se prueba cambiando sameSite a lax

--- src/controllers/users/users.controller.js
+++ src/controllers/users/users.controller.js
@@ -96,7 +96,7 @@ export class UsersController {
                               maxAge: 60 * 60 * 1000,
                               httpOnly: true,
                               secure: false,
-                              sameSite: 'none',
+                              sameSite: 'lax',
                         });
 
                         req.message = `Usuario ${userToLogin.email}, encontrado correctamente`;
