51657813fe31b90675c1d4431916776a171c7df9
Dne <valentinalvarez1998@gmail.com>
2024-07-25 15:18:29 -0300

    Modificación de configuración de CORS

--- src/server.js
+++ src/server.js
@@ -32,12 +32,15 @@ app.use(cors({
       origin: function (origin, callback) {
             if (!origin) return callback(null, true);
             if (allowedOrigins.indexOf(origin) === -1) {
-                  var msg = 'The CORS policy for this site does not ' +
+                  const msg = 'The CORS policy for this site does not ' +
                         'allow access from the specified Origin.';
                   return callback(new Error(msg), false);
             }
             return callback(null, true);
-      }
+      },
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 app.use(cookieParser());
